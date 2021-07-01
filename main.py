1d84ae46b2012745e45630cf02b75f47fc1b64a0
xxxMAnDARAxxx <43121814+HeadNeck@users.noreply.github.com>
2021-07-01 13:39:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ from gatys import Gatys
 
 img = Image.open('img.jpg')
 img_style = Image.open('img_style.jpg')
+
 img = asarray(img)
 img_style = asarray(img_style)
 
 image_result = Gatys().transfer(img, img_style) # feed me 2 images and go brrr
-plt.imshow(image_result)
+
 image = Image.fromarray(image_result.astype('uint8'), 'RGB')
 image.save("result.jpg")
+
+plt.imshow(image_result)
