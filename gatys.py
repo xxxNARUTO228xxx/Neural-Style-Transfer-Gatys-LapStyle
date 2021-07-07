8422e3d7d9a09cbbda3b94322ad799f976163b70
xxxNARUTO_228xxx <44481414+xxxNARUTO228xxx@users.noreply.github.com>
2021-07-07 23:39:49 +0300

    Update gatys.py

--- gatys.py
+++ gatys.py
@@ -6,14 +6,11 @@ from tensorflow import keras
 
 def deprocess_img(processed_img):
     x = processed_img.copy()
-    if len(x.shape) == 4:
-        x = np.squeeze(x, 0)
-
+    x = np.squeeze(x, 0)
     x[:, :, 0] += 103.939
     x[:, :, 1] += 116.779
     x[:, :, 2] += 123.68
     x = x[:, :, ::-1]
-
     x = np.clip(x, 0, 255).astype('uint8')
     return x
 
